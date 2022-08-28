d55716291f2193e24484a82da4f369e4bc73c806
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-08-28 14:40:25 +0300

    commit 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Notiflix from 'notiflix';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const formEl = document.querySelector('#search-form');
+const inputEl = document.querySelector('input');
+const loadMoreBtn = document.querySelector('.load-more');
+const imgGallery = document.querySelector('.gallery');
+
---
+++ src/js/asiosAPI.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const url = 'https://pixabay.com/api/';
+const key = '29544184-f05b1a44cab73eff12533d9b3';
